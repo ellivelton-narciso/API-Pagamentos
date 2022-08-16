ed3447cf575c36927120ce198905db91bc8bf58a
Ellivelton Narciso <ellivelton.souza@gmail.com>
2022-08-16 07:48:16 -0400

    Estrutura Front End

--- FrontEnd/assets/js/main.js
+++ FrontEnd/assets/js/main.js
@@ -1,22 +1,50 @@
-var settings = {
-    "url": "https://api.mercadopago.com/checkout/preferences",
-    "method": "POST",
-    "timeout": 0,
-    "headers": {
-        "Authorization": "Bearer APP_USR-7595035943301502-081601-519f3f8200b0ad4805e49f76f854b917-226812792",
+$.ajax({
+    url: "https://api.mercadopago.com/checkout/preferences",
+    method: "POST",
+    headers: {
+        "Authorization": "Bearer ",
         "Content-Type": "application/json"
     },
-    "data": JSON.stringify({
+    data: JSON.stringify({
         "items": [
             {
+                "id": "produto1",
                 "title": "Meu produto",
                 "quantity": 1,
                 "unit_price": 75.76
             }
-        ]
+        ],
+        "payer": {
+            "first_name": "Test",
+            "last_name": "Test",
+            "phone": {
+                "area_code": 11,
+                "number": "987654321"
+            },
+            "address": {}
+        }
     }),
-};
+    beforeSend: function() {
 
-$.ajax(settings).done(function (response) {
-    console.log(response);
+    },
+    success: function (res){
+        console.log(res)
+    },
+    error: function () {
+
+    }
+});
+
+
+$.ajax({
+    url: "https://api.mercadopago.com/v1/payments/search?sort=date_created&criteria=desc",
+    method: "GET",
+    timeout: 0,
+    headers: {
+        "Authorization": "Bearer ",
+        "Content-Type": "application/json"
+    }
+    success: function (res){
+        console.log(res)
+    }
 });
