61f8f660d6d83c9be3e6f588d33b496d620d5053
Ellivelton Narciso <ellivelton.souza@gmail.com>
2022-08-18 14:51:14 -0400

    feat: :sparkles: Feito função para pegar token do Mercado Pago e mandar o cliente pro checkout do Mercado Pago.

--- FrontEnd/assets/js/main.js
+++ FrontEnd/assets/js/main.js
@@ -54,9 +54,9 @@ $('#enviarNome').click(()=>{
     const nomePersonagem = $('#nomePersonagem').val()
     $('.is-invalid').removeClass('is-invalid')
 
-    if(nomePersonagem == '') {
+    if(nomePersonagem === '' || nomePersonagem === null) {
         $('#nomePersonagem').addClass('is-invalid')
-    } if(nomePersonagem.split('_').length != 2) {
+    } if(nomePersonagem.split('_').length !== 2) {
         $('#nomePersonagem').addClass('is-invalid')
         const alertaNomeErrado = `<div class="mt-lg-3 col-lg-8 alert alert-danger alert-dismissible fade show" role="alert">
             O nome deve estar no formato Nome_Sobrenome.
@@ -97,18 +97,38 @@ function geraProduto(idProduto, nomeProduto, precoProduto, beneficios) {
     if(Number.isInteger(precoProduto)) return JSON.stringify(precoProduto).replace('.', ',')
     if(Number.isFinite(precoProduto)) return JSON.stringify(precoProduto).toFixed(2).replace('.', ',')
     idProduto = idProduto.replace(' ', '').replace('"', '').replace("'", "").replace('<', '').replace('/', '')
-    const cardProduto = `<div class="col-lg-3 cardProduto">
-        <div class="card carProduto_titulos" style="background-color: #818181; color: #ffffff; border: none">
+    const cardProduto = `
+    <style>
+    #`+ idProduto +`-`+ idProduto +`.cardProduto:hover{
+        transform: scale(1.05);
+    }
+    #carProduto_titulos-`+ idProduto +`{
+        -webkit-box-shadow: 0 -4px 15px 6px rgba(0,0,0,0.28);
+        box-shadow: 0 -4px 15px 6px rgba(0,0,0,0.28);
+        border-radius: 0.375rem 0.375rem  0 0!important;
+    }
+    #carProduto_beneficios-`+ idProduto +`{
+        -webkit-box-shadow: 0 8px 15px 6px rgba(0,0,0,0.28);
+        box-shadow: 0 8px 15px 6px rgba(0,0,0,0.28);
+        border-radius:   0 0 0.375rem 0.375rem !important;
+    }
+    #item_beneficios-`+ idProduto +` li{
+    list-style: none;
+    text-decoration: none;
+}
+    </style>
+    <div id="`+ idProduto +`-`+ idProduto +`" class="col-lg-3 cardProduto">
+        <div class="card" id="carProduto_titulos-`+ idProduto +`" style="background-color: #818181; color: #ffffff; border: none">
           <div class="row" style="padding: 30px 20px">
             <h5 class="tituloProduto" style="font-size: 0.8rem">`+ nomeProduto +`</h5>
             <h3 class="precoProduto">R$ `+ precoProduto +`</h3>
           </div>
         </div>
-        <div class="card carProduto_beneficios" style="border: none">
+        <div class="card" id="carProduto_beneficios-`+ idProduto +`" style="border: none">
           <div class="row" style="padding: 30px">
             <div class="col-lg-10">
               <div class="row">
-                <ul class="item_beneficios">
+                <ul id="item_beneficios-`+ idProduto+`">
                 </ul>
               </div>
             </div>
@@ -122,46 +142,65 @@ function geraProduto(idProduto, nomeProduto, precoProduto, beneficios) {
     let beneficiosLista
     for (let i = 0; i < beneficios.length; i++) {
         beneficiosLista = `<li><i class="fa-solid fa-circle-check text-success"></i> `+ beneficios[i] +`</li><hr>`
-        $(beneficiosLista).clone().appendTo('.item_beneficios')
+        $(beneficiosLista).clone().appendTo('#item_beneficios-'+ idProduto +'')
     }
 
-$('#'+ idProduto +'').click(()=>{
-    $.ajax({
-        url: "https://api.mercadopago.com/checkout/preferences",
-        method: "POST",
-        headers: {
-            "Authorization": "Bearer APP_USR-7595035943301502-081601-519f3f8200b0ad4805e49f76f854b917-226812792",
-            "Content-Type": "application/json"
-        },
-        data: JSON.stringify({
-            "items": [
-                {
-                    "id": idProduto,
-                    "title": "Meu produto",
-                    "quantity": 1,
-                    "unit_price": parseInt(precoProduto.replace(',', '.'))
-                }
-            ],
-            "payer": {
-                "first_name": "",
-                "last_name": "",
-                "phone": {
-                    "area_code": "",
-                    "number": ""
-                },
-                "address": {}
+    $('#'+ idProduto +'').click(()=>{
+        const idCliente = JSON.parse(localStorage.usuario).id
+        if(idCliente === null || idCliente === 0) return  logout()
+        $.ajax({
+            url: 'localhost:5000/compraFeita',
+            method: 'POST',
+            data: {
+                "id": idCliente,
+                "idProduto": idProduto
+            },
+            beforeSend: function () {
+                // loading
+            },
+            success: function (data) {
+                const tokenMP = data.token
+                $.ajax({
+                    url: "https://api.mercadopago.com/checkout/preferences",
+                    method: "POST",
+                    headers: {
+                        "Authorization": "Bearer "+ tokenMP +"",
+                        "Content-Type": "application/json"
+                    },
+                    data: JSON.stringify({
+                        "items": [
+                            {
+                                "id": idProduto,
+                                "title": nomeProduto,
+                                "quantity": 1,
+                                "unit_price": parseInt(precoProduto.replace(',', '.'))
+                            }
+                        ],
+                        "payer": {
+                            "first_name": "",
+                            "last_name": "",
+                            "phone": {
+                                "area_code": "",
+                                "number": ""
+                            },
+                            "address": {}
+                        }
+                    }),
+                    success: function (res){
+                        location.assign(res.init_point)
+                    },
+                    error: function () {
+
+                    }
+                });
             }
-        }),
-        beforeSend: function() {
+        })
 
-        },
-        success: function (res){
-            location.assign(res.init_point)
-        },
-        error: function () {
 
-        }
-    });
-
-})
+    })
 }
+
+geraProduto('coins', 'Coins', '1', []);
+geraProduto('vipBronze', 'Vip Bronze', '10,00', ['30 dias de vip', 'Tag no Discord de Vip', 'Tag Vip no servidor', '10 coins', '+1 veículo no inventário'])
+geraProduto('vipPrata', 'Vip Prata', '20,00', ['30 dias de vip', 'Tag no Discord de Vip', 'Tag Vip no servidor', '20 coins', '+2 veículo no inventário'])
+geraProduto('vipOuro', 'Vip Ouro', '30,00', ['30 dias de vip', 'Tag no Discord de Vip', 'Tag Vip no servidor', '40 coins', '+4 veículo no inventário'])
